f01f4b1fec20970d9c0d1f5a424b66f5f0208aaf
IVOTE\slobodan.stevkovski <slobodan.stevkovski@ivote.mk>
2018-10-03 14:58:06 +0200

    dashboard / map - MSA-207

--- wp-content/themes/mainstreet-advocates/dashboard.php
+++ wp-content/themes/mainstreet-advocates/dashboard.php
@@ -7,7 +7,6 @@
     }
 
 </style>
-
 <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.css" />
 <script src="https://www.amcharts.com/lib/3/ammap.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
@@ -16,413 +15,124 @@
 <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
 <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
 <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
-<div class="container">
-<div class="row">
-<select name="" id="" class="col-md-3"></select>
-<select name="" id="" class="col-md-3"></select>
-<select name="" id="" class="col-md-3"></select>
-</div>
-<div id="chartdiv"></div>
+<?php 
 
+    global $wpdb;
+
+    $cat_query="SELECT pname FROM `profile_match`";
+    $categories = $wpdb->get_results($cat_query,OBJECT );
+
+?>
+<div class="main">
+    <div class="row">
+        <div class="col-md-3">
+            <h2>Filter</h2>
+            <div class="form-group">
+                <select name="status" id="status">
+                <option value="0">Status</option>
+            </select>
+            </div>
+            <div class="form-group">
+                <select name="category" id="category">
+                    <option value="%">Category</option>
+                   <?php foreach ($categories as $category) { ?>
+                    <option value="<?php echo $category->pname ?>"><?php echo $category->pname ?></option>
+                   <?php } ?>
+            </select>
+            </div>
+            <div class="form-group">
+                <select name="priority" id="priority">
+                    <option value="%">Prioritized</option>
+                    <option value="1">Yes</option> 
+                    <option value="NULL">No</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div id="chartdiv"></div>
+        </div>
+    </div>
 </div>
 <?php 
-    while($x <= 5) {
-    echo '{
-                "id": "US-AL",
-                "modalUrl": "<?php echo get_site_url() ?>/legislation-list/",
-                "selectable": true,
-                "value": 500
-            },';
-    $x++;
-} 
+    $json = file_get_contents('http://localhost/msa_test/wp-content/themes/mainstreet-advocates/states.json');
+    $states=json_decode($json); 
 ?>
 <script>
-    var map = AmCharts.makeChart("chartdiv", {
-        "type": "map",
-        "theme": "light",
-        "colorSteps": 20,
-        "dataProvider": {
-            "map": "usaLow",
-            "areas": [{
-                "id": "US-AL",
-                "modalUrl": "<?php echo get_site_url() ?>/legislation-list/",
-                "selectable": true,
-                "value": 500
-            }, {
-                "id": "US-AK",
-                "modalUrl": "<?php echo get_site_url() ?>/legislation-list/",
-                "selectable": true,
-                "value": 555
-            }, {
-                "id": "US-AZ",
-                "modalUrl": "<?php echo get_site_url() ?>/legislation-list/",
-                "selectable": true,
-                "value": 999
-            }, {
-                "id": "US-AR",
-                "modalUrl": "<?php echo get_site_url() ?>/legislation-list/",
-                "selectable": true,
-                "value": 1500
-            }, {
-                "id": "US-CA",
-                "modalUrl": "<?php echo get_site_url() ?>/legislation-list/",
-                "selectable": true,
-                "value": 1500
-            }, {
-                "id": "US-CO",
-                "value": 1500
-            }, {
-                "id": "US-CT",
-                "value": 1500
-            }, {
-                "id": "US-DE",
-                "value": 1500
-            }, {
-                "id": "US-FL",
-                "value": 1500
-            }, {
-                "id": "US-GA",
-                "value": 1500
-            }, {
-                "id": "US-HI",
-                "value": 1500
-            }, {
-                "id": "US-ID",
-                "value": 1500
-            }, {
-                "id": "US-IL",
-                "value": 1500
-            }, {
-                "id": "US-IN",
-                "value": 1500
-            }, {
-                "id": "US-IA",
-                "value": 1500
-            }, {
-                "id": "US-KS",
-                "value": 1500
-            }, {
-                "id": "US-KY",
-                "value": 1500
-            }, {
-                "id": "US-LA",
-                "value": 1500
-            }, {
-                "id": "US-ME",
-                "value": 1500
-            }, {
-                "id": "US-MD",
-                "value": 1500
-            }, {
-                "id": "US-MA",
-                "value": 1500
-            }, {
-                "id": "US-MI",
-                "value": 1500
-            }, {
-                "id": "US-MN",
-                "value": 1500
-            }, {
-                "id": "US-MS",
-                "value": 1500
-            }, {
-                "id": "US-MO",
-                "value": 1500
-            }, {
-                "id": "US-MT",
-                "value": 1500
-            }, {
-                "id": "US-NE",
-                "value": 1500
-            }, {
-                "id": "US-NV",
-                "value": 1500
-            }, {
-                "id": "US-NH",
-                "value": 1500
-            }, {
-                "id": "US-NJ",
-                "value": 1500
-            }, {
-                "id": "US-NM",
-                "value": 1500
-            }, {
-                "id": "US-NY",
-                "modalUrl": "https://en.wikipedia.org/wiki/China",
-                "selectable": true,
-                "value": 1
-            }, {
-                "id": "US-NC",
-                "value": 1500
-            }, {
-                "id": "US-ND",
-                "value": 1500
-            }, {
-                "id": "US-OH",
-                "value": 1500
-            }, {
-                "id": "US-OK",
-                "value": 1500
-            }, {
-                "id": "US-OR",
-                "value": 1500
-            }, {
-                "id": "US-PA",
-                "value": 1500
-            }, {
-                "id": "US-RI",
-                "value": 1500
-            }, {
-                "id": "US-SC",
-                "value": 1500
-            }, {
-                "id": "US-SD",
-                "value": 1500
-            }, {
-                "id": "US-TN",
-                "value": 1500
-            }, {
-                "id": "US-TX",
-                "value": 1500
-            }, {
-                "id": "US-UT",
-                "value": 1500
-            }, {
-                "id": "US-VT",
-                "value": 1500
-            }, {
-                "id": "US-VA",
-                "value": 1500
-            }, {
-                "id": "US-WA",
-                "value": 1500
-            }, {
-                "id": "US-WV",
-                "value": 1500
-            }, {
-                "id": "US-WI",
-                "value": 1500
-            }, {
-                "id": "US-WY",
-                "value": 1500
+    $(document).ready(function() {
+        
+    function myFunction(parameter) {  
+        var map = AmCharts.makeChart("chartdiv",{
+            "type": "map",
+            "theme": "light",
+            "colorSteps": 20,
+            "dataProvider": {
+                "map": "usaLow",
+                "areas": [
+                    //                looping through states
+                    <?php foreach ($states as $state) { ?> {
+                        "id": "US-<?php echo $state->abbreviation; ?>",
+                        "modalUrl": "<?php echo get_site_url() ?>/dashboard-list/?cat=" + parameter + "&st=<?php echo $state->abbreviation; ?>",
+                        "selectable": true,
+                        "value": 500
+                    },
+                    <?php  } ?>
+                ]
+            },
+            "areasSettings": {
+                "autoZoom": true,
+                "selectedColor": "#CC0000",
+                "rollOverColor": "#404040",
+                "rollOverOutlineColor": "#FFFFFF",
+            },
+            "export": {
+                "enabled": true
+            },
+            "valueLegend": {
+                "right": 20,
+                "minValue": "little",
+                "maxValue": "a lot!"
+            },
+            "listeners": [{
+                "event": "clickMapObject",
+                "method": function(event) {
+                    $.fancybox({
+                        "href": event.mapObject.modalUrl,
+                        "title": event.mapObject.title,
+                        "type": "iframe",
+                        'width': '80%',
+                        'height': '80%'
+                    });
+                    
+                }
             }]
-        },
-        "areasSettings": {
-            "autoZoom": true,
-            "selectedColor": "#CC0000",
-            "rollOverColor": "#404040",
-            "rollOverOutlineColor": "#FFFFFF",
-        },
-        "export": {
-            "enabled": true
-        },
-        "valueLegend": {
-            "right": 20,
-            "minValue": "little",
-            "maxValue": "a lot!"
-        },
-        "listeners": [{
-            "event": "clickMapObject",
-            "method": function(event) {
-                $.fancybox({
-                    "href": event.mapObject.modalUrl,
-                    "title": event.mapObject.title,
-                    "type": "iframe",
-                    'width': '80%',
-                    'height': '80%'
-                });
-            }
-        }]
+        });
+        
+    }
+        
+        var category;
+        var priority;
+        var parameter;
+        
+        myFunction();
+
+        $('#category').on('change', function() {
+            category = this.value;
+            priority = $('#priority').val();
+            parameter =  category + '&pr='+ priority;
+            myFunction(parameter);
+        });
+        
+         $('#priority').on('change', function() {
+            priority = this.value;
+            category= $('#category').val();
+            parameter =  category + '&pr='+ priority;
+            myFunction(parameter);
+        });
+
+    
+        
+    
+
     });
 
 </script>
-<!--
-<script>
-var map = AmCharts.makeChart( "chartdiv", {
-  "type": "map",
-  "theme": "light",
-  "colorSteps": 20,
-
-  "dataProvider": {
-    "map": "usaLow",
-    "areas": [ {
-      "id": "US-AL",
-      "modalUrl": "https://en.wikipedia.org/wiki/United_States",
-      "selectable": true,
-      "value": 16932
-    }, {
-      "id": "US-AK",
-      "value": 99626932
-    }, {
-      "id": "US-AZ",
-      "value": 5130632
-    }, {
-      "id": "US-AR",
-      "value": 2673400
-    }, {
-      "id": "US-CA",
-      "value": 33871648
-    }, {
-      "id": "US-CO",
-      "value": 4301261
-    }, {
-      "id": "US-CT",
-      "value": 3405565
-    }, {
-      "id": "US-DE",
-      "value": 783600
-    }, {
-      "id": "US-FL",
-      "value": 15982378
-    }, {
-      "id": "US-GA",
-      "value": 8186453
-    }, {
-      "id": "US-HI",
-      "value": 1211537
-    }, {
-      "id": "US-ID",
-      "value": 1293953
-    }, {
-      "id": "US-IL",
-      "value": 12419293
-    }, {
-      "id": "US-IN",
-      "value": 6080485
-    }, {
-      "id": "US-IA",
-      "value": 2926324
-    }, {
-      "id": "US-KS",
-      "value": 2688418
-    }, {
-      "id": "US-KY",
-      "value": 4041769
-    }, {
-      "id": "US-LA",
-      "value": 4468976
-    }, {
-      "id": "US-ME",
-      "value": 1274923
-    }, {
-      "id": "US-MD",
-      "value": 5296486
-    }, {
-      "id": "US-MA",
-      "value": 6349097
-    }, {
-      "id": "US-MI",
-      "value": 9938444
-    }, {
-      "id": "US-MN",
-      "value": 4919479
-    }, {
-      "id": "US-MS",
-      "value": 2844658
-    }, {
-      "id": "US-MO",
-      "value": 5595211
-    }, {
-      "id": "US-MT",
-      "value": 902195
-    }, {
-      "id": "US-NE",
-      "value": 1711263
-    }, {
-      "id": "US-NV",
-      "value": 1998257
-    }, {
-      "id": "US-NH",
-      "value": 1235786
-    }, {
-      "id": "US-NJ",
-      "value": 8414350
-    }, {
-      "id": "US-NM",
-      "value": 1819046
-    }, {
-      "id": "US-NY",
-      "value": 18976457
-    }, {
-      "id": "US-NC",
-      "value": 8049313
-    }, {
-      "id": "US-ND",
-      "value": 642200
-    }, {
-      "id": "US-OH",
-      "value": 11353140
-    }, {
-      "id": "US-OK",
-      "value": 3450654
-    }, {
-      "id": "US-OR",
-      "value": 3421399
-    }, {
-      "id": "US-PA",
-      "value": 12281054
-    }, {
-      "id": "US-RI",
-      "value": 1048319
-    }, {
-      "id": "US-SC",
-      "value": 4012012
-    }, {
-      "id": "US-SD",
-      "value": 754844
-    }, {
-      "id": "US-TN",
-      "value": 5689283
-    }, {
-      "id": "US-TX",
-      "value": 20851820
-    }, {
-      "id": "US-UT",
-      "value": 2233169
-    }, {
-      "id": "US-VT",
-      "value": 608827
-    }, {
-      "id": "US-VA",
-      "value": 7078515
-    }, {
-      "id": "US-WA",
-      "value": 5894121
-    }, {
-      "id": "US-WV",
-      "value": 1808344
-    }, {
-      "id": "US-WI",
-      "value": 5363675
-    }, {
-      "id": "US-WY",
-      "value": 493782
-    } ]
-  },
-
-  "areasSettings": {
-    "autoZoom": true,
-    "selectedColor": "#CC0000"
-  },
-
-  "valueLegend": {
-    "right": 10,
-    "minValue": "little",
-    "maxValue": "a lot!"
-  },
-  "listeners": [{
-    "event": "clickMapObject",
-    "method": function(event) {
-      $.fancybox({
-        "href": event.mapObject.modalUrl,
-        "title": event.mapObject.title,
-        "type": "iframe"
-      });
-    }
-  }],
-
-  "export": {
-    "enabled": true
-  }
-
-} );</script>-->
---
+++ wp-content/themes/mainstreet-advocates/legislation_list_dashboard.php
@@ -0,0 +1,158 @@
+<?php /* Template Name: Legislation_list_dashboard */ ?>
+<?php get_header(); ?>
+
+<?php 
+    $cat = $_GET["cat"];
+    $state = $_GET["st"];
+    $priority = $_GET["pr"];
+
+//var_dump($cat);
+//var_dump($state);
+//var_dump($priority);
+?>
+
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+
+<?php 
+    global $wpdb;
+
+    if($cat ==='undefined' ) {
+        $cat='%';  
+    } 
+
+    if($priority ==='null' ) {
+        $priority='%';  
+    } 
+
+    $client=esc_attr( get_the_author_meta( 'company', get_current_user_id()) );
+
+    $query = "SELECT * , profile_match.pname FROM `legislation` 
+            INNER join profile_match on legislation.id = profile_match.entity_id    
+            where profile_match.client='$client' 
+            and profile_match.entity_type='legislation'
+            and legislation.state='$state'
+            and profile_match.pname like '$cat'
+            and legislation.isPriority like '$priority'
+            group by profile_match.entity_id order by legislation.id ASC";
+
+    $legData = $wpdb->get_results($query);
+
+?>
+
+<div class="main" style="margin-left:5%; width:90%">
+  <h2>Legislations lists</h2>       
+  <table id="legislation" class="table table-striped">
+    <thead>
+      <tr>
+        <th>Priority</th>
+        <th>Session</th>
+        <th>State</th> 
+        <th>Type</th>
+        <th>Number</th>
+        <th>Sponsor(s)</th>
+        <th>Title</th>
+        <th>Abstract</th>
+        <th>Updated</th>
+        <th>Status</th>
+        <th>Categorie(s)</th> 
+        <th>Latest Action</th>
+      </tr>
+    </thead>
+    <tbody>
+     <?php foreach ($legData as $data) {
+       ?>
+      <tr>
+       <td style="text-align: center;">
+          <?php if($data->isPriority === '1'){ ?> 
+                <i class="star fa fa-star fa-lg" aria-hidden="true" id="<?php echo $data->number; ?>"></i>
+           <?php } else { ?>
+               <i class="star fa fa-star-o fa-lg" aria-hidden="true" id="<?php echo $data->number; ?>"></i>
+           <?php } ?>
+       </td>
+        <td><?php echo $data->session; ?></td>
+        <td><?php echo $data->state; ?></td>
+        <td><?php echo $data->type; ?></td>  
+        <td><?php echo $data->number; ?></td>
+        <td><?php echo $data->sponsor_name; ?></td>
+        <td><?php echo $data->title; ?></td> 
+        <td><?php echo $data->abstract; ?></td>
+        <td></td>
+        <td></td>
+        <td><?php echo $data->pname; ?></td>
+        <td></td>
+      </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</div>
+<?php get_footer(); ?>
+<script>
+$(document).ready( function () {
+    
+// * Call up datatables */
+   $('#legislation').DataTable( {
+        dom: 'Bfrtip',
+        buttons: ['print',
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [ 0, ':visible' ]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'csvHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            'colvis'
+        ]
+    } );
+    
+    
+// * Mark as priority */
+    $( ".star" ).click(function() {
+    
+        var id = $(this).attr('id');
+        $(this).toggleClass('fa-star fa-star-o');
+
+       $.ajax({
+                    type: "POST",
+                    url: "/msa_test/wp-content/themes/mainstreet-advocates/add-priority.php",
+                    data: ({value:id}),
+                    success: function(data) {
+                    }
+                });
+
+    });
+
+    
+} );
+</script>
