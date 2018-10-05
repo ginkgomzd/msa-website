39da59100f26fbe73c784a890da98d40b466bc25
IVOTE\snezana.smilkova <snezana.smilkova@ivote.mk>
2018-10-05 13:18:32 +0200

    Styling in details page

--- wp-content/themes/mainstreet-advocates/template_detailed_view.php
+++ wp-content/themes/mainstreet-advocates/template_detailed_view.php
@@ -10,46 +10,80 @@
         <!--       main  content -->
         <div class="col-md-9">
             <section id="main-info">
-                <h3><?php echo $row->state.' '.$row->type .' ' .$row->number ; ?>  </h3>
-                
-                <?php if($row->isPriority === '1'){ ?>
-                     <i class="star fa fa-star fa-lg" aria-hidden="true"></i>
-                <?php } ?> 
-                
-                <p>
-                    <span><strong>Sponsors:</strong></span>
-                    <span><a href="<?php echo $row->sponsor_url; ?>" target="_blank"><?php echo $row->sponsor_name; ?></a></span>
-                </p>
-                <p>
-                    <span><strong>Session:</strong></span>
-                    <span><?php echo $row->session; ?></span>
-                </p>
-                <p>
-                    <span><strong>Title:</strong></span>
-                    <span><?php echo $row->title; ?></span>
-                </p>
-                <p>
-                    <span><strong>Abstract:</strong></span>
-                    <span><?php echo $row->abstract; ?></span>
-                </p>
-                <p>
-                    <span><strong>Categorie(s):</strong></span>
-                    <span><?php echo getCategories($id) ?></span>
-                </p>
-                <p>
-                    <span><strong>Keyword(s):</strong></span>
-                    <span><?php echo getKeyword($id) ?> </span>
-                </p>
-                <p>
-                    <span><strong>Latest action:</strong></span>
-                    <span>2015 - 2016</span>
-                </p>
+                <h3 class="mb-3">
+                   <?php if($row->isPriority === '1'){ ?>
+                <i class="star fa fa-star" aria-hidden="true"></i>
+                <?php } ?>
+                   
+                   
+                    <?php echo $row->state.' '.$row->type .' ' .$row->number ; ?> </h3>
+
+               <hr class="w-25 ml-0">
+                <table>
+                    <tr>
+                        <td>
+                          Sponsors  
+                        </td>
+                        <td>
+                          <a href="<?php echo $row->sponsor_url; ?>" target="_blank"><?php echo $row->sponsor_name; ?></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          Session  
+                        </td>
+                        <td>
+                          <?php echo $row->session; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          Title  
+                        </td>
+                        <td>
+                          <?php echo $row->title; ?>  
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          Abstract  
+                        </td>
+                        <td>
+                          <?php echo $row->abstract; ?>  
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          Categorie(s)  
+                        </td>
+                        <td>
+                          <?php echo getCategories($id) ?>  
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          Keyword(s) 
+                        </td>
+                        <td>
+                          <?php echo getKeyword($id) ?> 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                          Latest action 
+                        </td>
+                        <td>
+                          2015 - 2016
+                        </td>
+                    </tr>
+                    
+                </table>
             </section>
             <br>
             <section id="latest-action">
                 <h3>Latest Action</h3>
                 <div class="progress" style="height:40px">
-                    <div class="progress-bar bg-success" role="progressbar" style="width:10%; height:40px">
+                    <div class="progress-bar" role="progressbar" style="width:10%; height:40px">
                         1
                     </div>
                     <div class="progress-bar" role="progressbar" style="width:10%; height:40px">
@@ -72,91 +106,60 @@
             </section>
             <br>
             <section id="notes">
-                <div class="row">
-                    <div class="col-md-9">
-                        <h3>Notes</h3>
-                    </div>
-                    <div class="col-md-3">
-                        <button id="addNote" type="button" class="btn btn-primary btn-sm ">Add note</button>
-                    </div>
-                </div>
-                <div class="notes">
+                        <h3 class="d-inline">Notes</h3>
+                        <button id="addNote" type="button" class="float-right button gradient-bg  btn-small"><i class="fa fa-plus" aria-hidden="true"></i> Add note</button>
+                <div class="notes mt-3">
                     <textarea rows="4" cols="50" name="note1"></textarea>
                 </div>
-                    <button id="sendNote" type="button" class="btn btn-primary">Send</button>
+                <button id="sendNote" type="button" class="button gradient-bg">Send</button>
             </section>
         </div>
 
         <!--       sidebar -->
         <div class="col-md-3">
-            <div class="row">
-                <div class="social">
-                    <i class="fab fa-facebook-square fa-2x"></i>
-                    <i class="fab fa-twitter fa-2x"></i>
-                    <i class="far fa-envelope fa-2x"></i>
+                <div class="social mb-3">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="far fa-envelope"></i></a>
                 </div>
-            </div>
-            <br>
             <div>
-               <?php if($row->textUploaded === '1'){ ?>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">View Full Bill Text</button>
+                <?php if($row->textUploaded === '1'){ ?>
+                <button type="button" class="button gradient-bg  btn-small mb-4 w-100" data-toggle="modal" data-target="#myModal">View Full Bill Text</button>
                 <?php } ?>
             </div>
             <br>
-            <section id="Additional-info">
+            <section id="Additional-info" class="contact-info mb-5">
+               <h2>Info</h2>
+                <br>
                 <h5>Additional Session Info</h5>
                 <br>
-                <p>
-                    <span><strong>State:</strong></span>
-                    <span><?php echo $row->state; ?></span>
-                </p>
-                <p>
-                    <span><strong>Convenes:</strong></span>
-                    <span>01/01/2018</span>
-                </p>
-                <p>
-                    <span><strong>Adjoums:</strong></span>
-                    <span>01/01/2018</span>
-                </p>
-                <p>
-                    <span><strong>Cary over:</strong></span>
-                    <span>No</span>
-                </p>
-                <p>
-                    <span><strong>Prefilling:</strong></span>
-                    <span>No</span>
-                </p>
-                <p>
-                    <span><strong>Additional info:</strong></span>
-                    <span>No2016 regular sesion</span>
-                </p>
+                <ul class="p-0 m-0">
+                        <li>State:<span><?php echo $row->state; ?></span></li>
+                        <li>Convenes: <span>01/01/2018</span></li>
+                        <li>Adjoums: <span>01/01/2018</span></li>
+                        <li>Cary over: <span>No</span></li>
+                        <li>Prefilling: <span>No</span></li>
+                        <li>Additional info: <span>No2016 regular sesion</span></li>
+                    </ul>
+                    
+                    
+                    
+                    
             </section>
             <br>
-            <section id="Additional-info">
-                <h5>Hearings</h5>
+            <section id="Additional-info" class="opening-hours">
+                <h2>Hearings</h2>
                 <br>
                 <div class="hearings">
-                    <h6>Hearing title</h6>
-                    <p>
-                        <span><strong>Place:</strong></span>
-                        <span>test</span>
-                    </p>
-                    <p>
-                        <span><strong>Date:</strong></span>
-                        <span>01/01/2018</span>
-                    </p>
-                    <p>
-                        <span><strong>Time:</strong></span>
-                        <span>01:00 PM</span>
-                    </p>
-                    <p>
-                        <span><strong>Committee:</strong></span>
-                        <span>Senate </span>
-                    </p>
-                    <p>
-                        <span><a href="#">Add to calendar</a></span>
-                        <span><i class="far fa-calendar-alt"></i></span>
-                    </p>
+                    <h5>Hearing title</h5>
+                    <ul class="p-0 m-0">
+                        <li>Place<span>test</span></li>
+                        <li>Date <span>01/01/2018</span></li>
+                        <li>Time <span>01:00 PM</span></li>
+                        <li>Committee <span>Senate</span></li>
+                    </ul>
+                    <a href="#" class="button gradient-bg btn-small mt-4 w-100 text-center"><i class="far fa-calendar-alt"></i> Add to calendar</a>
+
 
                 </div>
             </section>
@@ -169,34 +172,34 @@
  $row_text=$wpdb->get_row($txtQuery,OBJECT);
 ?>
 
-<!-- The Modal -->
-<style>
-    .modal-xl{
-        max-width:1440px;
-        
-    }
-    .modal-content{
-        padding: 20px;
-    }
-</style>
-<div class="modal fade bd-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
-    <div class="modal-content">
-      <?php echo $row_text->content ?>
+    <!-- The Modal -->
+    <style>
+        .modal-xl {
+            max-width: 1440px;
+
+        }
+
+        .modal-content {
+            padding: 20px;
+        }
+    </style>
+    <div class="modal fade bd-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <?php echo $row_text->content ?>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-<?php get_footer() ?>
-<script>
-    $(document).ready(function() {
-        
-        var x = '<textarea rows="4" cols="50" name="note1"></textarea>';
-        
-        $("#addNote").click(function() {
-            $( ".notes" ).append(x);
+    <?php get_footer() ?>
+    <script>
+        $(document).ready(function() {
+
+            var x = '<textarea rows="4" cols="50" name="note1"></textarea>';
+
+            $("#addNote").click(function() {
+                $(".notes").append(x);
+            });
+
+
         });
-
-
-    });
-
-</script>
+    </script>
