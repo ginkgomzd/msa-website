374ca995bc1e029b98dc9ffe601ad988aa02f8e7
IVOTE\slobodan.stevkovski <slobodan.stevkovski@ivote.mk>
2019-01-30 08:29:45 +0100

    remove old

--- wp-content/plugins/feed_parser/msa-feed-parser-master/curl-feeds.mk
+++
@@ -1,9 +0,0 @@
-
-feed-upload-url := http://msa.ginkgostreet.com/feeds/index.php
-
-curl-feeds:
-	IFS=$$'\n'; \
-	for f in `ls -1`; do \
-		curl -F "data=@$$(realpath $$f)" -F"filename=$$(basename $$f)" $(feed-upload-url); \
-		sleep 3; \
-	done
