2b7b54a5bac11e3719cc466f95e3da5c443eba63
ginkgoMZD <mzd@ginkgostreet.com>
2019-03-27 00:13:12 -0400

    pruning

--- wp-content/themes/mainstreet-advocates/handle-session-disable.php
+++
@@ -1,7 +0,0 @@
-<?php
-include( '../../../wp-load.php' );
-global $wpdb;
-
-$wpdb->query('UPDATE session_info 
-SET is_active = 0 
-WHERE end_date <  DATE_ADD(NOW(),INTERVAL 1 MONTH) AND is_active = 1');
